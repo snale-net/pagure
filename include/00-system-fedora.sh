d8fac8af8a3cf0255c22e57356e4fcb70e80e47d
Fabien Rétif <fabien.retif@zoho.com>
2019-12-29 22:10:48 +0100

    fix os includes

--- include/00-system-fedora.sh
+++ include/00-system-fedora.sh
@@ -2,8 +2,13 @@
 
 sudo dnf install gcc gcc-c++ gcc-fortran
 sudo dnf install tcl tcl-devel tcllib
-sudo dnf install python-devel freetype-devel libpng-devel libX11-devel libXaw-devel m4 patch make cmake expat-devel autoconf bison flex libcurl-devel
+sudo dnf install freetype-devel libpng-devel libX11-devel libXaw-devel m4 patch make cmake expat-devel autoconf bison flex libcurl-devel
+# Python v2.x
+sudo dnf install python-devel
+# Python v3.x
+# GDAL deps
 sudo dnf install libsqlite3x-devel
+# GMT Man deps
 
 
 
--- include/00-system-suse.sh
+++ include/00-system-suse.sh
@@ -2,7 +2,7 @@
 
 sudo zypper install gcc gcc-c++ gcc-fortran git
 sudo zypper install tcl tcl-devel tcllib
-sudo zypper install freetype-devel freetype2-devel python-pybind11-devel libpng-devel libX11-devel libXaw-devel m4 patch make cmake libexpat-devel autoconf bison flex libcurl-devel
+sudo zypper install freetype-devel freetype2-devel libpng-devel libX11-devel libXaw-devel m4 patch make cmake libexpat-devel autoconf bison flex libcurl-devel
 # Python v2.x
 sudo zypper install python-devel
 # Python v3.x
