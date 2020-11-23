da4418a8ce5d4e6764783314e901c631caebd7d7
Fabien Rétif <fabien.retif@zoho.com>
2020-11-23 12:01:20 +0100

    remove old script

--- install-java.sh
+++
@@ -1,4 +0,0 @@
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/java/jre1.8.0_241/bin/javaws" 40
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/java/jre1.8.0_241/bin/java" 40
-sudo  update-alternatives --config java
-sudo ln -s /usr/java/jre1.8.0_241/lib/amd64/libnpjp2.so /usr/lib/browser-plugins/
