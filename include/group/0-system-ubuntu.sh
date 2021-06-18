75975a3f95aa42296dfebb395c904581bf04bea7
Fabien Rétif <fabien.retif@zoho.com>
2021-06-18 09:38:25 +0200

    Fix: Add dos2unix to Ubuntu & Mint

--- include/group/0-system-ubuntu.sh
+++ include/group/0-system-ubuntu.sh
@@ -11,6 +11,7 @@ fi
 sudo apt-get install $aptArgs gfortran g++ git 2>&1 >&3 | tee -a $LOGFILE && leave 
 sudo apt-get install $aptArgs tcl tcl-dev tcllib 2>&1 >&3 | tee -a $LOGFILE && leave
 sudo apt-get install $aptArgs libfreetype6-dev libjpeg-dev libpng-dev libx11-dev libxaw7-dev m4 patch make cmake autoconf bison flex libcurl4-gnutls-dev liblzma-dev libbz2-dev 2>&1 >&3 | tee -a $LOGFILE && leave
+sudo apt-get install $aptArgs dos2unix 2>&1 >&3 | tee -a $LOGFILE && leave
 
 if [ "$pythonInterpreter" != "none" ]; then # only-if-Python
 	if (( $(echo "$pythonVersion >= 3.0" | bc -l) )); then # only Python>=3.0
