7542b283c47408d6ae7c33092a876ebc04dcf132
Fabien Rétif <fabien.retif@zoho.com>
2021-06-30 15:30:51 +0200

    Fix: change scons module name to Scons

--- include/group/3-python-modules.sh
+++ include/group/3-python-modules.sh
@@ -460,7 +460,7 @@ args["$group-$index"]=""
 
 # scons-4.1.0
 index=27
-name["$group-$index"]=scons
+name["$group-$index"]=SCons
 version["$group-$index"]=4.1.0
 details["$group-$index"]=""
 url["$group-$index"]="https://files.pythonhosted.org/packages/be/d0/bf4e7003369c6d8a6e490741c54791c7918d9ef10b56aec201e76706f1d7/SCons-4.1.0.post1.tar.gz"
