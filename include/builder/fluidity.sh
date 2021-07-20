017cca48b818811cd2c23cae9c6a4e1d088adc2f
Fabien Rétif <fabien.retif@zoho.com>
2021-07-20 23:55:39 +0200

    fluidity remove make clean

--- include/builder/fluidity.sh
+++ include/builder/fluidity.sh
@@ -2,7 +2,7 @@
 
 cp libspud/ltmain.sh . 2>&1 >&3 | tee -a $LOGFILE && leave
 echo $"${args["$index"]}" | xargs ./configure --prefix=$prefix/${dirinstall["$index"]} --libdir=$prefix/${dirinstall["$index"]}/lib 2>&1 >&3 | tee -a $LOGFILE && leave
-make -j clean  2>&1 >&3 | tee -a $LOGFILE && leave
+#make -j clean  2>&1 >&3 | tee -a $LOGFILE && leave
 make -j 2>&1 >&3 | tee -a $LOGFILE && leave
 make -j fltools 2>&1 >&3 | tee -a $LOGFILE && leave
 make -j install 2>&1 >&3 | tee -a $LOGFILE && leave
