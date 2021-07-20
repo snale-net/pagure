44c16271a1e70ba53ee7f579cdb46c9ae7acf7d3
Fabien Rétif <fabien.retif@zoho.com>
2021-07-20 23:39:30 +0200

    fluidity builder add copy of ltmain.sh

--- include/builder/fluidity.sh
+++ include/builder/fluidity.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp libspud/ltmain.sh . 2>&1 >&3 | tee -a $LOGFILE && leave
 echo $"${args["$index"]}" | xargs ./configure --prefix=$prefix/${dirinstall["$index"]} --libdir=$prefix/${dirinstall["$index"]}/lib 2>&1 >&3 | tee -a $LOGFILE && leave
 make -j clean  2>&1 >&3 | tee -a $LOGFILE && leave
 make -j 2>&1 >&3 | tee -a $LOGFILE && leave
