7ee8fbd19307a340df6a03808537dbb676a165a9
Fabien Rétif <fabien.retif@zoho.com>
2021-07-21 10:40:50 +0200

    add autogen to h5hut for fluidity builder

--- include/builder/fluidity.sh
+++ include/builder/fluidity.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cp libspud/ltmain.sh . 2>&1 >&3 | tee -a $LOGFILE && leave
+cd h5hut
+./autogen.sh 2>&1 >&3 | tee -a $LOGFILE && leave
+cd ..
 echo $"${args["$index"]}" | xargs ./configure --prefix=$prefix/${dirinstall["$index"]} --libdir=$prefix/${dirinstall["$index"]}/lib 2>&1 >&3 | tee -a $LOGFILE && leave
 #make -j clean  2>&1 >&3 | tee -a $LOGFILE && leave
 make -j 2>&1 >&3 | tee -a $LOGFILE && leave
