26c756453de5c60e3abc888f3336801a14998f29
Fabien Rétif <fabien.retif@zoho.com>
2021-07-23 16:47:01 +0200

    fluidity change numpy include

--- include/builder/scipy.sh
+++ include/builder/scipy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d "$prefix/${dirinstall["$index"]}/bin" ] ; then mkdir -p "$prefix/${dirinstall["$index"]}/bin" 2>&1 >&3 | tee -a $LOGFILE && leave; fi
+if [ ! -d "$prefix/${dirinstall["$index"]}/include/$pythonInterpreter" ] ; then mkdir -p "$prefix/${dirinstall["$index"]}/include/$pythonInterpreter" 2>&1 >&3 | tee -a $LOGFILE && leave; fi
+if [ ! -d "$prefix/${dirinstall["$index"]}/lib/$pythonInterpreter/site-packages" ] ; then mkdir -p "$prefix/${dirinstall["$index"]}/lib/$pythonInterpreter/site-packages" 2>&1 >&3 | tee -a $LOGFILE && leave; fi
+
 if [[  "$compiler" == "intel" ]] ; then					
 	$pythonInterpreter setup.py config --compiler=intelem --fcompiler=intelem build_clib --compiler=intelem --fcompiler=intelem build_ext --compiler=intelem --fcompiler=intelem install --user --force 2>&1 >&3 | tee -a $LOGFILE && leave 
 else                   
