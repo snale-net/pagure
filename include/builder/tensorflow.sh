3df7611c313e8fd81798e2ded50c2c39de1d66ea
Fabien Rétif <fabien.retif@zoho.com>
2022-03-13 15:53:25 +0100

    Fix multi args param

--- include/builder/tensorflow.sh
+++ include/builder/tensorflow.sh
@@ -10,7 +10,8 @@ bazel clean 2>&1 >&3 | tee -a $LOGFILE && leave
 
 sed -i "1i #!/usr/bin/env ${pythonInterpreter}" tensorflow/tools/git/gen_git_source.py 2>&1 >&3 | tee -a $LOGFILE && leave
 
-echo ${args["$index"]} | xargs -i sh -c "bazel build {} //tensorflow/tools/pip_package:build_pip_package" 2>&1 >&3 | tee -a $LOGFILE && leave
+cmd=`echo ${args["$index"]} | xargs -0 -i echo bazel build {} //tensorflow/tools/pip_package:build_pip_package | sed -z '$ s/\n//' | xargs`
+exec $cmd 2>&1 >&3 | tee -a $LOGFILE && leave
 
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package ./tensorflow_pkg 2>&1 >&3 | tee -a $LOGFILE && leave
 pip install ./tensorflow_pkg/tensorflow-${version["$index"]}-cp37-cp37m-linux_x86_64.whl 2>&1 >&3 | tee -a $LOGFILE && leave
