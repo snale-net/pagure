79f5fcd44a84fcbf2b34bbaf1d8b53dff0d32cb4
Fabien Rétif <fabien.retif@zoho.com>
2022-06-28 13:43:22 +0200

    Add python 3.9

--- include/group/1-python.sh
+++ include/group/1-python.sh
@@ -53,5 +53,52 @@ prepend-path CPATH $prefix/${dirinstall["$group-$index"]}/include/python${python
 
 fi # end-only Python==3.7
 
+if (( $(echo "$pythonVersion == 3.9" |bc -l) )); then # only Python==3.9
+
+#Python-3.9.13
+index=2
+name["$group-$index"]=python
+version["$group-$index"]=3.9
+details["$group-$index"]=""
+url["$group-$index"]=https://www.python.org/ftp/python/3.9.13/Python-3.9.13.tgz
+filename["$group-$index"]=Python-3.9.13.tgz
+dirname["$group-$index"]=Python-3.9.13
+builder["$group-$index"]="python-builder"
+#dependencies["$group-$index"]=""
+dirinstall["$group-$index"]="${name["$group-$index"]}/$compilo/${version["$group-$index"]}"
+if [[ $compiler == "intel" ]]; then
+	args["$group-$index"]="--enable-shared --without-gcc --with-icc"
+else
+	args["$group-$index"]="--enable-shared"
+fi
+dirmodule["$group-$index"]="${name["$group-$index"]}/$compilo"
+modulefile["$group-$index"]="#%Module1.0                                                                                                                                                                                                                                 
+proc ModulesHelp { } {                                                                                                                                                                                                                      
+global dotversion
+ 
+puts stderr \"\t$(tr '[:lower:]' '[:upper:]' <<< ${name["$group-$index"]:0:1})${name["$group-$index"]:1} ${version["$group-$index"]}\"
+}
+ 
+module-whatis \"$(tr '[:lower:]' '[:upper:]' <<< ${name["$group-$index"]:0:1})${name["$group-$index"]:1} ${version["$group-$index"]}\"
+
+# Dependencies
+module load dependencies_modules
+
+# Variables
+prepend-path PATH $prefix/${dirinstall["$group-$index"]}/bin
+prepend-path LD_LIBRARY_PATH $prefix/${dirinstall["$group-$index"]}/lib
+prepend-path LIBRARY_PATH $prefix/${dirinstall["$group-$index"]}/lib
+prepend-path MANPATH $prefix/${dirinstall["$group-$index"]}/share/man
+prepend-path C_INCLUDE_PATH  $prefix/${dirinstall["$group-$index"]}/include
+prepend-path INCLUDE $prefix/${dirinstall["$group-$index"]}/include
+prepend-path CPATH $prefix/${dirinstall["$group-$index"]}/include
+prepend-path C_INCLUDE_PATH  $prefix/${dirinstall["$group-$index"]}/include/python${pythonVersion}m
+prepend-path INCLUDE $prefix/${dirinstall["$group-$index"]}/include/python${pythonVersion}m
+prepend-path CPATH $prefix/${dirinstall["$group-$index"]}/include/python${pythonVersion}m
+"
+
+fi # end-only Python==3.9
+
+
 fi  # only-if-we-want-Python-and-is-not-installed 
 
