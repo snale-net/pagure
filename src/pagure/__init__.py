c16d45e9ee355b2c24d8fb65d9be3322eab6438b
Fabien Rétif <fabien.retif@zoho.com>
2025-06-18 14:22:48 +0200

    Refactor & cleanup the code

---
+++ src/pagure/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+__version__ = "2.0.0"
+
+
+def main(args: list[str] | None = None) -> int:
+    """This is an internal API only meant for use by pagure's own console scripts.
+    """
+    from pagure.utils.entrypoints import _wrapper
+
+    return _wrapper(args)
