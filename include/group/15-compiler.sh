c91e2264fad78745b7e13c12155d2d8e3cd83e84
Fabien Rétif <fabien.retif@zoho.com>
2025-10-15 11:27:35 +0200

    Add GCC 7.5.0

---
+++ include/group/15-compiler.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+#################################################################
+#Group 15 : Compilers
+group="15"
+groupname[$group]="Compilers"
+
+# mpc 1.3.1
+index=1
+name["$group-$index"]=mpc
+version["$group-$index"]=1.3.1
+details["$group-$index"]=""
+url["$group-$index"]="https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz" 
+filename["$group-$index"]=mpc-1.3.1.tar.gz
+dirname["$group-$index"]=mpc-1.3.1
+builder["$group-$index"]="configure"
+dependencies["$group-$index"]="gmp/$compilo/6.2.1 mpfr/$compilo/4.1.0"
+dirinstall["$group-$index"]="${name["$group-$index"]}/$compilo/${version["$group-$index"]}"
+args["$group-$index"]=""
+dirmodule["$group-$index"]="${name["$group-$index"]}/$compilo"
+modulefile["$group-$index"]="#%Module1.0
+proc ModulesHelp { } {
+global dotversion
+ 
+puts stderr \"\t$(tr '[:lower:]' '[:upper:]' <<< ${name["$group-$index"]:0:1})${name["$group-$index"]:1} ${version["$group-$index"]}\"
+}
+ 
+module-whatis \"$(tr '[:lower:]' '[:upper:]' <<< ${name["$group-$index"]:0:1})${name["$group-$index"]:1} ${version["$group-$index"]}\"
+
+# Dependencies
+module load dependencies_modules
+
+# Variables
+prepend-path LD_LIBRARY_PATH $prefix/${dirinstall["$group-$index"]}/lib
+prepend-path LIBRARY_PATH $prefix/${dirinstall["$group-$index"]}/lib
+prepend-path MANPATH $prefix/${dirinstall["$group-$index"]}/share/man
+prepend-path C_INCLUDE_PATH $prefix/${dirinstall["$group-$index"]}/include
+prepend-path INCLUDE $prefix/${dirinstall["$group-$index"]}/include 
+prepend-path CPATH $prefix/${dirinstall["$group-$index"]}/include 
+"
+
+# isl 0.24
+index=2
+name["$group-$index"]=isl
+version["$group-$index"]=0.24
+details["$group-$index"]=""
+url["$group-$index"]="https://mirror.snale.net/isl-0.24.tar.bz2" 
+filename["$group-$index"]=isl-0.24.tar.bz2
+dirname["$group-$index"]=isl-0.24
+builder["$group-$index"]="configure"
+dependencies["$group-$index"]=""
+dirinstall["$group-$index"]="${name["$group-$index"]}/$compilo/${version["$group-$index"]}"
+args["$group-$index"]=""
+dirmodule["$group-$index"]="${name["$group-$index"]}/$compilo"
+modulefile["$group-$index"]="#%Module1.0
+proc ModulesHelp { } {
+global dotversion
+ 
+puts stderr \"\t$(tr '[:lower:]' '[:upper:]' <<< ${name["$group-$index"]:0:1})${name["$group-$index"]:1} ${version["$group-$index"]}\"
+}
+ 
+module-whatis \"$(tr '[:lower:]' '[:upper:]' <<< ${name["$group-$index"]:0:1})${name["$group-$index"]:1} ${version["$group-$index"]}\"
+
+# Dependencies
+module load dependencies_modules
+
+# Variables
+prepend-path LD_LIBRARY_PATH $prefix/${dirinstall["$group-$index"]}/lib
+prepend-path LIBRARY_PATH $prefix/${dirinstall["$group-$index"]}/lib
+prepend-path C_INCLUDE_PATH $prefix/${dirinstall["$group-$index"]}/include
+prepend-path INCLUDE $prefix/${dirinstall["$group-$index"]}/include 
+prepend-path CPATH $prefix/${dirinstall["$group-$index"]}/include 
+"
+
+# gcc 7.5.0
+index=3
+name["$group-$index"]=gcc
+version["$group-$index"]=7.5.0
+details["$group-$index"]=""
+url["$group-$index"]="https://mirror.snale.net/gcc-7.5.0.tar.gz" 
+filename["$group-$index"]=gcc-7.5.0.tar.gz
+dirname["$group-$index"]=gcc-7.5.0
+builder["$group-$index"]="configure"
+dependencies["$group-$index"]="gmp/$compilo/6.2.1 mpfr/$compilo/4.1.0 mpc/$compilo/1.3.1 isl/$compilo/0.24"
+dirinstall["$group-$index"]="${name["$group-$index"]}/$compilo/${version["$group-$index"]}"
+args["$group-$index"]="--enable-languages=c,c++,fortran --enable-bootstrap --disable-multilib"
+dirmodule["$group-$index"]="${name["$group-$index"]}/$compilo"
+modulefile["$group-$index"]="#%Module1.0
+proc ModulesHelp { } {
+global dotversion
+ 
+puts stderr \"\t$(tr '[:lower:]' '[:upper:]' <<< ${name["$group-$index"]:0:1})${name["$group-$index"]:1} ${version["$group-$index"]}\"
+}
+ 
+module-whatis \"$(tr '[:lower:]' '[:upper:]' <<< ${name["$group-$index"]:0:1})${name["$group-$index"]:1} ${version["$group-$index"]}\"
+
+# Variables
+prepend-path PATH $prefix/${dirinstall["$group-$index"]}/bin
+prepend-path LD_LIBRARY_PATH $prefix/${dirinstall["$group-$index"]}/lib
+prepend-path LIBRARY_PATH $prefix/${dirinstall["$group-$index"]}/lib
+prepend-path MANPATH $prefix/${dirinstall["$group-$index"]}/share/man
+prepend-path C_INCLUDE_PATH $prefix/${dirinstall["$group-$index"]}/include
+prepend-path INCLUDE $prefix/${dirinstall["$group-$index"]}/include 
+prepend-path CPATH $prefix/${dirinstall["$group-$index"]}/include 
+"
