86687be6f70651efc480037c48e727429a2301a2
Fabien Rétif <fabien.retif@zoho.com>
2025-10-27 14:56:12 +0100

    Change ubuntu package distutils to setuptools

--- include/group/0-system-ubuntu.sh
+++ include/group/0-system-ubuntu.sh
@@ -18,7 +18,7 @@ if [ "$pythonInterpreter" != "none" ]; then # only-if-Python
 	if (( $(echo "$pythonVersion >= 3.0" | bc -l) )); then # only Python>=3.0
 		# Python v3.x
 		sudo apt-get $aptArgs install python3-dev libffi-dev 2>&1 >&3 | tee -a $LOGFILE && leave
-		sudo apt-get $aptArgs install python3-distutils 2>&1 >&3 | tee -a $LOGFILE && leave
+		sudo apt-get $aptArgs install python3-setuptools 2>&1 >&3 | tee -a $LOGFILE && leave
 		# GMT deps
 		sudo apt-get $aptArgs install python3-sphinx 2>&1 >&3 | tee -a $LOGFILE && leave
 	else
